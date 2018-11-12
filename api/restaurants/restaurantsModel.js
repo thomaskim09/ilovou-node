d41dd3660e9676b654c43749500f5cbdaee57e09
Thomas0921 <thomaskim092150@gmail.com>
2018-11-12 19:28:47 +0800

    restaurantVouchers Trying

--- api/restaurants/restaurantsModel.js
+++ api/restaurants/restaurantsModel.js
@@ -23,87 +23,103 @@ const restaurantSchema = mongoose.Schema({
     currency: String,
     currencyCode: String,
     routineRestDay: Number,
-    businiessHours: [{
-      day: String,
-      openTime: Date,
-      closeTime: Date
-    }],
+    businiessHours: [
+      {
+        day: String,
+        openTime: Date,
+        closeTime: Date
+      }
+    ],
     contact: String,
     hasMenu: Boolean,
     hasReservation: Boolean
   },
-  vouchers: [{
-    voucherId: mongoose.Schema.Types.ObjectId,
-    voucherImage: String,
-    voucherName: String,
-    voucherType: String,
-    suitablePax: Number,
-    newPrice: mongoose.Schema.Types.Decimal128,
-    basePrice: mongoose.Schema.Types.Decimal128,
-    quantitySold: Number,
-    limitedQuantity: Number, // (optional)
-    soldOutTime: Date, // (optional) sold out time comes with the limited quantity
-    limitQuantityPerUser: Number, //(optional)
-    userReachedLimitQuantity: [
-      // User who exceed max quantity, will not view the voucher in voucher page
-      {
-        userId: String,
-        quantityBrought: Number
-      }
-    ],
-    limitedEndTime: Date, //(optional)
-    grabStartTime: Date, //(optional)
-    groupVoucherDetails: [{
-      groupQuantity: Number,
-      groupPricePerUnit: mongoose.Schema.Types.Decimal128
-    }],
-    quantityDetails: {
-      // This field is for Quantity Voucher
-      quantityTitle: String,
-      quantityContents: [{
-        itemName: String,
-        itemNewPrice: mongoose.Schema.Types.Decimal128,
-        itemPreviousPrice: mongoose.Schema.Types.Decimal128
-      }]
-    },
-    setDetails: [{
-      setTitle: String,
-      setContents: [{
-        setName: String,
-        setUnit: Number,
-        setPrice: mongoose.Schema.Types.Decimal128
-      }]
-    }],
-    voucherRules: {
-      validFrom: Date,
-      validUntil: Date,
-      startHour: Date,
-      endHour: Date,
-      ruleDetails: [String]
-    },
-    status: String
-  }],
+  vouchers: [
+    {
+      voucherId: mongoose.Schema.Types.ObjectId,
+      voucherImage: String,
+      voucherName: String,
+      voucherType: String,
+      suitablePax: Number,
+      newPrice: mongoose.Schema.Types.Decimal128,
+      basePrice: mongoose.Schema.Types.Decimal128,
+      quantitySold: Number,
+      limitedQuantity: Number, // (optional)
+      soldOutTime: Date, // (optional) sold out time comes with the limited quantity
+      limitQuantityPerUser: Number, //(optional)
+      userReachedLimitQuantity: [
+        // User who exceed max quantity, will not view the voucher in voucher page
+        {
+          userId: String,
+          quantityBrought: Number
+        }
+      ],
+      limitedEndTime: Date, //(optional)
+      grabStartTime: Date, //(optional)
+      groupVoucherDetails: [
+        {
+          groupQuantity: Number,
+          groupPricePerUnit: mongoose.Schema.Types.Decimal128
+        }
+      ],
+      quantityDetails: {
+        // This field is for Quantity Voucher
+        quantityTitle: String,
+        quantityContents: [
+          {
+            itemName: String,
+            itemNewPrice: mongoose.Schema.Types.Decimal128,
+            itemPreviousPrice: mongoose.Schema.Types.Decimal128
+          }
+        ]
+      },
+      setDetails: [
+        {
+          setTitle: String,
+          setContents: [
+            {
+              setName: String,
+              setUnit: Number,
+              setPrice: mongoose.Schema.Types.Decimal128
+            }
+          ]
+        }
+      ],
+      voucherRules: {
+        validFrom: Date,
+        validUntil: Date,
+        startHour: Date,
+        endHour: Date,
+        ruleDetails: [String]
+      },
+      status: String
+    }
+  ],
   reservation: {
     reservationSettings: {
       maxReservationDay: Number,
-      holidays: [{
-        holidayName: String,
-        holidayDate: Date
-      }],
+      holidays: [
+        {
+          holidayName: String,
+          holidayDate: Date
+        }
+      ],
       paxSettings: {
         minPax: Number,
         maxPax: Number
       }
     },
-    reservationDetails: [{
-      reservationId: mongoose.Schema.Types.ObjectId,
-      userId: mongoose.Schema.Types.ObjectId,
-      reservationDate: Date,
-      reservationTime: Date,
-      dinningPax: Number,
-      dinningArea: String,
-      remark: String, // (optional)
-    }]
+    reservationDetails: [
+      {
+        reservationId: mongoose.Schema.Types.ObjectId,
+        userId: mongoose.Schema.Types.ObjectId,
+        reservationDate: Date,
+        reservationTime: Date,
+        dinningPax: Number,
+        dinningArea: String,
+        remark: String // (optional)
+      }
+    ]
   },
   // feedBack: [{
   //     userId: String,
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port, function(){
-    console.log('Running on port: ' + port)
+server.listen(port, function() {
+  console.log("Running on port: " + port);
 });
